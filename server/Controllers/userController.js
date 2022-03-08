95f5c43303e64f5e7029e0dde1d159b4330c7ac2
ahmed <ahmedlearn01@gmail.com>
2022-03-08 12:06:10 -0800

    registration

--- server/Controllers/userController.js
+++ server/Controllers/userController.js
@@ -1,14 +1,40 @@
+const jwt = require('jsonwebtoken');
+const User = require('../Models/userModel');
+const asyncHandler = require('express-async-handler');
+
 // registration
-const userRegister = (req,res) =>{
-    res.status(200).json({message: 'here for registration :) '})
-}
+const userRegister =asyncHandler(async (req,res) =>{
+    const {name, email, password} = req.body;
+
+    if(!name || !email || !password){
+        res.status(400);
+        throw new Error('please complete all fields ...')
+    }
+
+    // check if not registered already
+
+    const existedUser = await User.findOne({email});
+    if(existedUser){
+        res.status(400);
+        throw new Error('user already existed');
+    }
+
+    res.status(200).json({message: genJwt('salfjskfj3332232ddkal')})
+})
+
 // login User
-const loginUser = (req,res) =>{
+const loginUser =asyncHandler(async (req,res) =>{
     res.status(200).json({message: 'here for login page /\:) '})
-}
+})
+
 // user profile
-const userProfile = (req,res) =>{
+const userProfile =asyncHandler(async (req,res) =>{
     res.status(200).json({message: 'here for user profile info :) '})
+})
+
+// Generate jwt
+const genJwt = (id) => {
+    return jwt.sign(id,process.env.JWT_SEC);
 }
 
 module.exports = {
