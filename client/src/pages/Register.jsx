435b167fdab8888183ccfb84989ed365e0e9f7ad
ahmed <ahmedlearn01@gmail.com>
2022-06-19 10:13:24 -0700

    form inputs error

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,25 +1,15 @@
 import {useState, useEffect} from 'react'
 
 const Register = () => {
-    const [data, setData] = useState({
-        name: '',
-        email: '',
-        password: '',
-        password2: '',
-    })
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [password2, setPassword2] = useState('');
 
     const submitForm = (e) =>{
         e.preventDefault();
-
-        console.log(e.target.name.value)
-
-        // setData({
-        //     name: e.target.name.value,
-        //     email: e.target.email.value,
-        //     password: e.target.password.value,
-        //     password2: e.target.name.value,
-        // })
-        // console.log(data)
+        const data = {...name, email,password,password2}
+        console.log(data);
     }
   return (
     <div className='register'>
@@ -30,22 +20,34 @@ const Register = () => {
                 <input type="text" 
                     placeholder='Name'
                     name="name"
-                    required/>
+                    required
+                    value={name}
+                    onChange={(e)=>setName(e.target.name.value)}
+                    />
             </div>
             <div className="input-group">
                 <input type="email" 
                     placeholder='Email'
-                    name="email" required/>
+                    name="email" required
+                    value={email}
+                    onChange={(e)=>setEmail(e.target.email.value)}
+                    />
             </div>
             <div className="input-group">
                 <input type="password" 
                     placeholder='Password'
-                    name="password" required/>
+                    name="password" required
+                    value={password}
+                    onChange={(e)=>setPassword(e.target.password.value)}
+                    />
             </div>
             <div className="input-group">
                 <input type="password" 
                     placeholder='Confirm password'
-                    name="password2" required/>
+                    name="password2" required
+                    value={password2}
+                    onChange={(e)=>setPassword2(e.target.password2.value)}
+                    />
             </div>
             <button type='submit'>Submit</button>
         </form>
