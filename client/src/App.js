52db621098f2cda803e521c4bb260d1f30231a10
ahmed <ahmedlearn01@gmail.com>
2022-06-19 09:12:07 -0700

    routes are ready

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,26 @@ import Header from "./components/Header";
 import Blogs from "./pages/Blogs";
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './styles/main.scss'
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 function App() {
   return (
-    <>
-      <Header />
-      <Blogs />
-    </>
+    <Router>
+      <>
+        <Header />
+        <Routes>
+          <Route path="/"  element={
+          <>
+            <Blogs />
+          </>
+        }/>
+          <Route path="/register" element={<Register />}/>
+          <Route path={'/login'} element={<Login />}/>
+        </Routes>
+      </>
+    </Router>
+    
   );
 }
 
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,12 @@
-
+import {Link} from 'react-router-dom';
 
 const Header = () => {
   return (
     <div className='header'>
-        <h1>Somblogs</h1>
+        <h1><a href="/">Somblogs</a></h1>
         <div className="links">
-          <a href={'/register'} >Register</a>
-          <a to={'/login'}>Sig in</a>
+          <Link to={'/register'} >Register</Link>
+          <Link to={'/login'}>Sig in</Link>
         </div>
     </div>
   )
