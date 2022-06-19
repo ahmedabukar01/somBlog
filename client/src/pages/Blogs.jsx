2e47c457f021924559ef365ad56ed0616c04c859
ahmed <ahmedlearn01@gmail.com>
2022-06-19 16:58:44 -0700

    unknown stage

--- client/src/pages/Blogs.jsx
+++ client/src/pages/Blogs.jsx
@@ -1,9 +1,32 @@
-import Header from "../components/Header"
+import axios from 'axios';
+import {useState, useEffect} from 'react'
+
+
 
 const Blogs = () => {
+  const [blogs,setBlogs] = useState([]);
+
+  useEffect(()=>{
+    const fetchData = async () =>{
+      try {
+        console.log('hi')
+        const res = await fetch('http://localhost:3000/posts');
+        const posts = await res.json();
+
+        console.log(posts)
+        
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    fetchData();
+  },[])
+
   return (
     <div className='blogs'>
        <h1>All Blogs...</h1>
+
     </div>
   )
 }
