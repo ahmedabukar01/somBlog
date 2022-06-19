f7833753cbce3b40151a0d8b876fb03728f89456
ahmed <ahmedlearn01@gmail.com>
2022-06-19 09:40:30 -0700

    registeration forms

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -7,30 +7,45 @@ const Register = () => {
         password: '',
         password2: '',
     })
+
+    const submitForm = (e) =>{
+        e.preventDefault();
+
+        console.log(e.target.name.value)
+
+        // setData({
+        //     name: e.target.name.value,
+        //     email: e.target.email.value,
+        //     password: e.target.password.value,
+        //     password2: e.target.name.value,
+        // })
+        // console.log(data)
+    }
   return (
     <div className='register'>
         <h1>Register</h1>
         <p>Welcome please create new account</p>
-        <form className='form'>
+        <form className='form' onSubmit={submitForm}>
             <div className="input-group">
                 <input type="text" 
                     placeholder='Name'
-                    name="name"/>
+                    name="name"
+                    required/>
             </div>
             <div className="input-group">
                 <input type="email" 
                     placeholder='Email'
-                    name="email"/>
+                    name="email" required/>
             </div>
             <div className="input-group">
                 <input type="password" 
                     placeholder='Password'
-                    name="password"/>
+                    name="password" required/>
             </div>
             <div className="input-group">
                 <input type="password" 
                     placeholder='Confirm password'
-                    name="password2"/>
+                    name="password2" required/>
             </div>
             <button type='submit'>Submit</button>
         </form>
