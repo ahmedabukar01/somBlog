6507655f8ad21bbdf85a802423f6d8d9ccaa83ed
unknown <ahmedlearn01@gmail.com>
2022-07-16 17:05:45 +0300

    redux setup

--- client/src/features/authSlice.js
+++ client/src/features/authSlice.js
@@ -1,1 +1,25 @@
 import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
+
+const user = localStorage.getItem('user');
+
+const initialState = {
+    user: user ? user : null,
+    isError: false,
+    isSuccess: false,
+    isLoading: false,
+    message: ''
+}
+
+export const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {
+        reset: (state) =>{
+
+        }
+    }
+})
+
+
+export const {reset} = authSlice.actions;
+export default authSlice.reducer
