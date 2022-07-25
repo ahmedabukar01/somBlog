e50b1abda41ed68b9f33a6e69507699cff41f7f7
ahmed <ahmedlearn01@gmail.com>
2022-07-25 09:16:12 +0300

    save user input

--- client/src/components/AddPost.jsx
+++ client/src/components/AddPost.jsx
@@ -1,16 +1,29 @@
 import {Button, Form, Container} from 'react-bootstrap'
+import {useState } from 'react'
 
 const AddPost = () => {
+    const [postData, setPostData] = useState({
+        title: '',
+        body: '',
+    })
+    const dataHandler = (e)=>{
+        setPostData(prevData => ({
+            ...prevData, [e.target.name]: e.target.value
+        }) )
+    }
+    
+    const { title, body} = postData;
+    console.log(title,body)
   return (
     <Container className="my-4">
         <Form>
             <Form.Group>
                 <Form.Label>Title</Form.Label>
-                <Form.Control type='text' placeholder='Title'/>
+                <Form.Control type='text' placeholder='Title' name="title" onChange={dataHandler}/>
             </Form.Group>
             <Form.Group>
                 <Form.Label>Body</Form.Label>
-                <Form.Control type='text' placeholder='Body'/>
+                <Form.Control type='text' placeholder='Body' name="body" onChange={dataHandler}/>
             </Form.Group>
             <Form.Group className="my-3 ">
                 <Button variant="success" type="submit">Post</Button>
